6033309c452cc464fe8d9e8ecd3dadc37515fb9d
dipendrashahithakuri <thakuridipendrashahi@gmail.com>
2017-07-08 00:30:48 +0545

    setting up static page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+
+    class StaticPagesController < ApplicationController
+  def about
+
+  end
+
+  def random
+    @idea = Idea.order("RANDOM()").first
+  end
+end
+end
