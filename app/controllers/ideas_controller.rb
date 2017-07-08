be426f070a01e5c62a7f700e41bd8b6ec349d62a
dipendrashahithakuri <thakuridipendrashahi@gmail.com>
2017-07-08 19:26:46 +0545

    Flash message added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,35 +1,42 @@
 class IdeasController < ApplicationController
-    def index
-        @ideas = Idea.all
-    end
-    
-     def create
-        @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
-        redirect_to root_path
-    end
+	def index
+    @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+  end
 
-    def edit
-        @idea = Idea.find(params[:id])    
-    end
+  def create    
+      @idea = Idea.create(idea_params)
+      if @idea.valid?
+         flash[:success] = "Your idea has been posted!"
+      else
+         flash[:alert] = "Woops! Looks like there has been an error!"
+      end
+      redirect_to root_path
+      end
 
-    def update
-        @idea = Idea.find(params[:id])
-        if @idea.update(idea_params)
-            redirect_to root_path
-        else
-            redirect_to edit_idea_path(params[:id])
-        end
-    end
+	def edit 
+		@idea = Idea.find(params[:id])
+	end
 
-    def destroy
-        @idea = Idea.find(params[:id])
-        @idea.destroy
-        redirect_to root_path
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Woops! Looks like there has been an error!"
+      redirect_to edit_idea_path(params[:id])
     end
+  end
 
-     private
-
-    def idea_params
-        params.require(:idea).permit(:description, :author)
-    end
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    flash[:success] = "The idea was successfully deleted!"
+    redirect_to root_path
+  end
+	
+	private 
+	def idea_params
+		params.require(:idea).permit(:description,:author)
+	end  
 end
